29029e5a6eb8685f5b13353d70d99d231d157226
Shubham_INT <shubham.shrivastava@indusnet.co.in>
2022-03-25 17:40:44 +0530

    multiplied

--- new_file.py
+++ new_file.py
@@ -1,6 +1,6 @@
 
 
 new_list = [1,3,4,5,6]
-print(new_list)
-print("adding git practice")
-print("adding git practice")
+print(2*new_list)
+# print("adding git practice")
+# print("adding git practice")
